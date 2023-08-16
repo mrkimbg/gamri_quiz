4c97b16beeb15180c56082be1e7779b304f09bd7
mrkimbg <32232980+mrkimbg@users.noreply.github.com>
2023-08-16 23:54:00 +0900

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,74 +1,57 @@
 import streamlit as st
 import pandas as pd
 import os
-import datetime
+import io
+from datetime import datetime
 
-# Set the title of the web app
+def check_answers(user_answers, correct_answers):
+    results = []
+    for ua, ca in zip(user_answers, correct_answers):
+        results.append("정답" if ua == ca else "오답")
+    return results
+
 st.title(':clap:정보시스템 감리사 기출풀이:smile:')
 
-# File uploader allows user to upload their own file
-uploaded_files = st.sidebar.file_uploader(":tada:기출문제 파일 업로드:file_folder:", type=['xlsx'], accept_multiple_files=True)
+uploaded_file = st.file_uploader("문제 엑셀 파일을 업로드해주세요.", type=['xlsx'])
 
-if 'start_time' not in st.session_state:
-    st.session_state['start_time'] = None
+if uploaded_file:
+    df = pd.read_excel(uploaded_file)
+    user_answers = []
 
-if uploaded_files:
-    file_list = [uploaded_file.name for uploaded_file in uploaded_files]
-    selected_file = st.sidebar.selectbox(":file_folder:파일을 선택:point_right:하세요", file_list)
-    for uploaded_file in uploaded_files:
-        if uploaded_file.name == selected_file:
-            # Remove the file extension from the selected file name
-            file_name, file_extension = os.path.splitext(selected_file)
-            
-            # Set the title with the selected file name
-            st.markdown(f"#### \"{file_name}\"을 선택하셨습니다.")
+    for index, row in df.iterrows():
+        st.write(f"문제 {index + 1}: {row['문제']}")
+        options = ["선택하세요."] + [row['보기1'], row['보기2'], row['보기3'], row['보기4']]
+        answer = st.radio(f"문제 {index + 1}의 답 선택:", options)
 
-            # Start exam button
-            if st.button('**시험 시작**'):
-                st.session_state['start_time'] = datetime.datetime.now()
-                st.write('시험을 시작했습니다!')
+        if answer != "선택하세요.":
+            user_answers.append(options.index(answer))
+        else:
+            user_answers.append(None)
 
-            if st.session_state['start_time'] is not None:
-                df = pd.read_excel(uploaded_file)
+    if st.button("제출하기"):
+        # 사용자 답을 1 기반으로 변환 (즉, 0 -> 1, 1 -> 2, ...)
+        df['사용자 답'] = [ua + 1 if ua is not None else "선택하지 않음" for ua in user_answers]
+        df['결과'] = check_answers(user_answers, df['답'].tolist())
+        
+        st.subheader("결과 확인")
+        
+        total_correct = df['결과'].tolist().count("정답")
+        
+        for index, row in df.iterrows():
+            st.write(f"문제 {index + 1}: {row['문제']}")
+            st.write(f"정답: {row['답']}, 사용자 답: {row['사용자 답']}, 결과: {row['결과']}")
+        
+        st.write(f"총 {len(df)}문제 중 {total_correct}개가 정답입니다.")
 
-                # Create a dictionary to store user answers
-                user_answers = {}
+        # 현재 월, 일, 시간을 가져온다.
+        now = datetime.now()
+        current_time = now.strftime("%m%d%H%M")
 
-                for index, row in df.iterrows():
-                    # Display each question and choices with markdown for consistency
-                    st.markdown(f'**문제 {index + 1}**')
-                    st.markdown(row["문제"])
+        # 파일명 생성
+        file_name = os.path.splitext(uploaded_file.name)[0] + "_" + current_time + '.xlsx'
 
-                    options = [None, f'1) {row["보기1"]}', f'2) {row["보기2"]}', f'3) {row["보기3"]}', f'4) {row["보기4"]}']
-                    answer = st.radio('보기 선택:', options, key=str(index) + uploaded_file.name)
-
-                    # Save the user's answer (Extract the number only from the answer)
-                    if answer is not None:
-                        user_answers[index] = int(answer[0])  # The first character of the answer is the number of the choice
-
-                # Submit button
-                if st.button('**제출 -** ' + uploaded_file.name):
-                    end_time = datetime.datetime.now()
-                    time_taken = end_time - st.session_state['start_time']
-
-                    # Convert time taken to minutes and seconds
-                    minutes, seconds = divmod(time_taken.total_seconds(), 60)
-
-                    # Get the current date in YYMMDD format
-                    current_date = end_time.strftime('%y%m%d')
-
-                    # Compare user answers with correct answers and save the results to the dataframe
-                    df['제출'] = df.index.map(user_answers)
-                    df['결과'] = df.apply(lambda row: 'O' if row['제출'] == row["답"] else 'X', axis=1)
-
-                    # Calculate the score
-                    score = (df['결과'] == 'O').sum()
-
-                    # Show the score and time taken
-                    st.write(f'당신의 점수는 {score} / {len(df)} 입니다. 시험에 걸린 시간은 {int(minutes)}분 {int(seconds)}초 입니다.')
-
-                    # Save the dataframe with the results to a new Excel file
-                    result_file_name = f'{file_name}_result_{current_date}_{int(minutes)}m_{int(seconds)}s.xlsx'
-                    df.to_excel(result_file_name, index=False)
-else:
-    st.write('기출문제:question: 파일을 :file_folder: 업로드 해주세요.')
+        # 엑셀 다운로드
+        towrite = io.BytesIO()
+        downloaded_file = df.to_excel(towrite, encoding='utf-8', index=False, engine='openpyxl')
+        towrite.seek(0)
+        st.download_button("결과 파일 다운로드", towrite, file_name, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
